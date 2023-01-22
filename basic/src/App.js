a0fbd640af8ca0153239f1822b9e8b0879027f0b
chinyday <chinyday@gmail.com>
2023-01-22 22:49:26 +0900

    feat: prettierrc/eslintrc setting

--- basic/src/App.js
+++ basic/src/App.js
@@ -1,29 +1,31 @@
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
-
-  const name = 'chinyday'
-  const milks = ['초코', '딸기', '바나나', '커피']
+  const name = "chinyday";
+  const milks = ["초코", "딸기", "바나나", "커피"];
 
   return (
     <>
       <h3>{`Hello, ${name}`}</h3>
-      {/* 화살표 함수, 리턴 사용x*/}
+      {/* 화살표 함수, 리턴 사용x */}
       <ul>
-        {
-          milks.map(item => <li>{item}</li>)
-        }
+        {milks.map((item, i) => (
+          <li key={i}>{item}</li>
+        ))}
       </ul>
       {/* 화살표 함수, 리턴 사용o */}
-      <ul>{
-        milks.map(item => { return <li>{item}</li> })
-      }</ul>
+      <ul>
+        {milks.map((item, i) => {
+          return <li key={i}>{item}</li>;
+        })}
+      </ul>
       {/* function 함수 */}
-      <ul>{
-        milks.map(function (item) {
-          return <li>{item}</li>
-        })
-      }</ul>
+      <ul>
+        {milks.map(function (item, i) {
+          return <li key={i}>{item}</li>;
+        })}
+      </ul>
     </>
   );
 }
