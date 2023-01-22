f9afb0593b493e7a06fd4e63edcc9def9d912525
chinyday <chinyday@gmail.com>
2023-01-22 21:20:42 +0900

    커밋 테스트

---
+++ basic/.eslintrc.js
@@ -0,0 +1,25 @@
+export default {
+  "env": {
+    "browser": true,
+    "es2021": true,
+    "commonjs": true,
+    "es6": true,
+    "node": true
+  },
+  "extends": [
+    "eslint:recommended",
+    "plugin:react/recommended",
+  ],
+  "overrides": [
+  ],
+  "parserOptions": {
+    "ecmaVersion": "latest",
+    "sourceType": "module"
+  },
+  "plugins": [
+    "react"
+  ],
+  "rules": {
+  },
+  "parser": "@babel/eslint-parser",
+}
--- basic/src/App.js
+++ basic/src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const name = 'chinyday'
+  const milks = ['초코', '딸기', '바나나', '커피']
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h3>{`Hello, ${name}`}</h3>
+      {/* 화살표 함수, 리턴 사용x*/}
+      <ul>
+        {
+          milks.map(item => <li>{item}</li>)
+        }
+      </ul>
+      {/* 화살표 함수, 리턴 사용o */}
+      <ul>{
+        milks.map(item => { return <li>{item}</li> })
+      }</ul>
+      {/* function 함수 */}
+      <ul>{
+        milks.map(function (item) {
+          return <li>{item}</li>
+        })
+      }</ul>
+    </>
   );
 }
 
