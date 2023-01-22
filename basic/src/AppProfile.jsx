f42a6e49859a0c8af6bb1c03df123ead0e10efa8
chinyday <chinyday@gmail.com>
2023-01-22 23:46:28 +0900

    feat: profile file setting

---
+++ basic/src/AppProfile.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import Profile from "./components/profile";
+
+function AppProfile() {
+  return (
+    <>
+      <Profile />
+    </>
+  );
+}
+
+export default AppProfile;
---
+++ basic/src/components/profile.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+function Profile() {
+  return <h3>Profile</h3>;
+}
+export default Profile;
