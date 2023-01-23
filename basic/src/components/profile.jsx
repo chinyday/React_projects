d80902720bca2df30fad80651bf8f311a1983318
chinyday <chinyday@gmail.com>
2023-01-23 23:49:24 +0900

    feat: new props 추가

--- basic/src/components/profile.jsx
+++ basic/src/components/profile.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "../App.css";
 
-function Profile({ image, name, job }) {
+function Profile({ image, name, job, isnew }) {
   return (
     <>
       <div className="profile">
         <img className="photo" src={image} alt="avatar" />
+        {/* {isnew ? <div className="new">new</div> : null} */}
+        {isnew && <div className="new">new</div>}
         <h3>{name}</h3>
         <h5>{job}</h5>
       </div>
