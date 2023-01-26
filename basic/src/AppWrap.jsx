eb408ec6c9654f07e121aec704d94930cb6b61e1
chinyday <chinyday@gmail.com>
2023-01-26 23:18:07 +0900

    feat: 컴포넌트 재사용

--- basic/src/AppWrap.jsx
+++ basic/src/AppWrap.jsx
@@ -1,29 +1,28 @@
 import React from "react";
 import "./App.css";
 
-function AppWrap() {
-  return <Navbar />;
-}
-
-function Navbar() {
+export default function AppWrap() {
   return (
-    <>
-      <header style={{ backgroundColor: "red" }}>
+    <div>
+      <Navbar>
         <Avatar
           image="https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80"
           name="James Kim"
-          size="200"
+          size={200}
         />
-      </header>
-    </>
+        <p>하잉</p>
+      </Navbar>
+      <Navbar>
+        <p>하잉</p>
+      </Navbar>
+    </div>
   );
 }
 
-function Avatar({ image, name, size }) {
-  return (
-    <>
-      <img className="photo" src={image} alt={name} width={size} />
-    </>
-  );
+function Navbar({ children }) {
+  return <header style={{ backgroundColor: "red" }}>{children}</header>;
 }
-export default AppWrap;
+
+function Avatar({ image, name, size }) {
+  return <img className="photo" src={image} alt={name} width={size} />;
+}
