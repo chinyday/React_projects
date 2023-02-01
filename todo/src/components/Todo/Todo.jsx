d6487e9dafd8f4066d6ed37d55f660a588e5ba04
chinyday <chinyday@gmail.com>
2023-02-01 16:09:55 +0900

    feat: 스타일링3

--- todo/src/components/Todo/Todo.jsx
+++ todo/src/components/Todo/Todo.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import {FaTrashAlt} from 'react-icons/fa';
+import stlyes from './Todo.module.css';
 
 function Todo({todo, onUpdate, onDelete}) {
 
@@ -13,10 +14,10 @@ function Todo({todo, onUpdate, onDelete}) {
   const handleDelete = () => onDelete(todo);
 
   return (
-    <li>
-      <input type="checkbox" id="checkbox" onChange={handleChange} checked={status === 'completed'} />
-      <label htmlFor="checkbox">{text}</label>
-      <button onClick={handleDelete}><FaTrashAlt /></button>
+    <li className={stlyes.todo}>
+      <input className={stlyes.checkbox} type="checkbox" id="checkbox" onChange={handleChange} checked={status === 'completed'} />
+      <label className={stlyes.text} htmlFor="checkbox">{text}</label>
+      <span className={stlyes.icon}><button className={stlyes.button} onClick={handleDelete}><FaTrashAlt /></button></span>
     </li>
   );
 }
