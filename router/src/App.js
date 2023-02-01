e883e0fd45ea0a8557bebcc4e08d7596f7b6ce9c
chinyday <chinyday@gmail.com>
2023-02-01 20:48:13 +0900

    feat: 기본적인 라우트 사용법

--- router/src/App.js
+++ router/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './App.css';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <p>Home</p>,
+    errorElement: <p>not found!</p>
+  },
+  {
+    path: '/vivi',
+    element: <p>vivi</p>,
+  },
+])
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <RouterProvider router={router}>app</RouterProvider>
 }
 
 export default App;
